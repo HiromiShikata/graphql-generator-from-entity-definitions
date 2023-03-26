ac9950d1a233ddfefc27a3d3181c0990f99059d2
HiromiShikata <shikata.hiromi@gmail.com>
2023-03-26 20:34:30 +0000

    feat: make cli entry point

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,147 @@
-import { hello } from './index';
+import { execSync } from 'child_process';
 
-describe('index', () => {
-  test('hello', async () => {
-    const res = hello('test');
-    expect(res).toEqual('hello test');
+describe('commander program', () => {
+  it('should output file contents', () => {
+    const output = execSync(
+      'npx ts-node ./src/index.ts ./src/adapter/repositories/_testdata -o ./tmp/schema.graphql',
+    ).toString();
+    expect(output.trim()).toBe(`enum ErrorCode {
+  UNKNOWN_RUNTIME
+  PERMISSION_DENIED
+  NOT_FOUND
+}
+
+type UnknownRuntimeError {
+  errorCode: ErrorCode!
+  message: String
+  stack: String
+}
+
+type PermissionDeniedError {
+  errorCode: ErrorCode!
+  message: String
+  stack: String
+}
+
+type NotFoundError {
+  errorCode: ErrorCode!
+  message: String
+  stack: String
+}
+
+type User {
+  id: String!
+  name: String!
+}
+
+
+union UserResult =
+    User
+  | NotFoundError
+  | PermissionDeniedError
+  | UnknownRuntimeError
+
+type UserList {
+  items: [User!]!
+  total: Int!
+}
+
+union UserListResult =
+    UserList
+  | NotFoundError
+  | PermissionDeniedError
+  | UnknownRuntimeError
+
+type Query {
+  user(id: ID!): UserResult!
+  userList(limit: Int, offset: Int): UserListResult!
+}
+
+input CreateUserInput {
+  name: String!
+  clientMutationId: String
+}
+
+type CreateUserPayload {
+  user: User!
+  clientMutationId: String
+}
+
+union CreateUserPayloadResult =
+    CreateUserPayload
+  | PermissionDeniedError
+  | UnknownRuntimeError
+
+input UpdateUserInput {
+  id: ID!
+  name: String!
+  clientMutationId: String
+}
+
+type UpdateUserPayload {
+  user: User!
+  clientMutationId: String
+}
+
+union UpdateUserPayloadResult =
+    UpdateUserPayload
+  | PermissionDeniedError
+  | UnknownRuntimeError
+  | NotFoundError
+
+input DeleteUserInput {
+  id: ID!
+  clientMutationId: String
+}
+
+type DeleteUserPayload {
+  id: ID!
+  clientMutationId: String
+}
+
+union DeleteUserPayloadResult =
+    DeleteUserPayload
+  | UnknownRuntimeError
+  | PermissionDeniedError
+  | NotFoundError
+
+type Mutation {
+  createUser(input: CreateUserInput!): CreateUserPayloadResult!
+  updateUser(input: UpdateUserInput!): UpdateUserPayloadResult!
+  deleteUser(input: DeleteUserInput!): DeleteUserPayloadResult!
+}`);
+  });
+
+  it('should output error message without input path', () => {
+    try {
+      execSync('npx ts-node ./src/index.ts').toString();
+      throw new Error(`should throw Error`);
+    } catch (error) {
+      if (
+        !error ||
+        typeof error !== 'object' ||
+        !('stderr' in error) ||
+        !(error.stderr instanceof Buffer)
+      ) {
+        throw error;
+      }
+
+      expect(error.stderr.toString()).toContain(
+        `error: missing required argument 'inputPath'`,
+      );
+    }
+  });
+  it('should output help', () => {
+    const output = execSync('npx ts-node ./src/index.ts -h').toString();
+    expect(output.trim()).toBe(`Usage: GraphQL Generator [options] <inputPath>
+
+Generate GraphQL schema from entity definitions
+
+Arguments:
+  inputPath                  Path to domain entities dir.
+
+Options:
+  -o, --output <outputPath>  Path to schema.graphql.
+  -h, --help                 display help for command`);
   });
 });
