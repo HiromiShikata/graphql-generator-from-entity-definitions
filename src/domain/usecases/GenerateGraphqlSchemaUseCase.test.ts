b9114cf91e54ce4a41bd9bd5e990f98e9bdf3c84
HiromiShikata <shikata.hiromi@gmail.com>
2023-03-26 18:19:58 +0000

    feat: create domain entities and GenerateGraphqlSchemaUseCase

---
+++ src/domain/usecases/GenerateGraphqlSchemaUseCase.test.ts
@@ -0,0 +1,767 @@
+// ./src/domain/usecases/GenerateGraphqlSchemaUseCase.test.ts
+import { GenerateGraphqlSchemaUseCase } from './GenerateGraphqlSchemaUseCase';
+import { FileRepository } from './adapter-interfaces/FileRepository';
+import { EntityDefinitionRepository } from './adapter-interfaces/EntityRelationDefinitionRepository';
+import { EntityDefinition } from '../entities/EntityDefinition';
+
+const entityDefinitions: EntityDefinition[] = [
+  {
+    typeName: 'User',
+    properties: [
+      { name: 'id', propertyType: 'string', isReference: false },
+      { name: 'name', propertyType: 'string', isReference: false },
+      {
+        name: 'deactivated',
+        propertyType: 'boolean',
+        isReference: false,
+      },
+    ],
+  },
+  {
+    typeName: 'Group',
+    properties: [
+      { name: 'id', propertyType: 'string', isReference: false },
+      { name: 'name', propertyType: 'string', isReference: false },
+    ],
+  },
+
+  {
+    typeName: 'UserGroup',
+    properties: [
+      { name: 'id', propertyType: 'string', isReference: false },
+      {
+        name: 'userId',
+        propertyType: 'User',
+        isReference: true,
+        isUnique: false,
+      },
+      {
+        name: 'groupId',
+        propertyType: 'Group',
+        isReference: true,
+        isUnique: false,
+      },
+    ],
+  },
+];
+
+describe('GenerateGraphqlSchemaUseCase', () => {
+  describe('run', () => {
+    it('should generate and save graphql schema file', async () => {
+      const { entityDefinitionRepository, fileRepository, useCase } =
+        createUseCaseAndMockRepositories();
+
+      entityDefinitionRepository.find.mockResolvedValue(entityDefinitions);
+      const domainEntitiesDirectoryPath = '/path/to/domain/entities';
+      const outputGraphqlSchemaPath = '/path/to/output/schema.graphql';
+
+      const expectedSchema = `
+enum ErrorCode {
+  UNKNOWN_RUNTIME
+  PERMISSION_DENIED
+  NOT_FOUND
+}
+
+type UnknownRuntimeError {
+  errorCode: ErrorCode!
+  message: String
+  stack: String
+}
+
+type PermissionDeniedError {
+  errorCode: ErrorCode!
+  message: String
+  stack: String
+}
+
+type NotFoundError {
+  errorCode: ErrorCode!
+  message: String
+  stack: String
+}
+
+type User {
+  id: String!
+  name: String!
+  deactivated: Boolean!
+  userGroupList: [UserGroupListResult!]!
+}
+
+type Group {
+  id: String!
+  name: String!
+  userGroupList: [UserGroupListResult!]!
+}
+
+type UserGroup {
+  id: String!
+  userId: String!
+  user: UserResult!
+  groupId: String!
+  group: GroupResult!
+}
+
+
+union UserResult =
+    User
+  | NotFoundError
+  | PermissionDeniedError
+  | UnknownRuntimeError
+
+type UserList {
+  items: [User!]!
+  total: Int!
+}
+
+union UserListResult =
+    UserList
+  | NotFoundError
+  | PermissionDeniedError
+  | UnknownRuntimeError
+
+union GroupResult =
+    Group
+  | NotFoundError
+  | PermissionDeniedError
+  | UnknownRuntimeError
+
+type GroupList {
+  items: [Group!]!
+  total: Int!
+}
+
+union GroupListResult =
+    GroupList
+  | NotFoundError
+  | PermissionDeniedError
+  | UnknownRuntimeError
+
+union UserGroupResult =
+    UserGroup
+  | NotFoundError
+  | PermissionDeniedError
+  | UnknownRuntimeError
+
+type UserGroupList {
+  items: [UserGroup!]!
+  total: Int!
+}
+
+union UserGroupListResult =
+    UserGroupList
+  | NotFoundError
+  | PermissionDeniedError
+  | UnknownRuntimeError
+
+type Query {
+  user(id: ID!): UserResult!
+  userList(limit: Int, offset: Int): UserListResult!
+  group(id: ID!): GroupResult!
+  groupList(limit: Int, offset: Int): GroupListResult!
+  userGroup(id: ID!): UserGroupResult!
+  userGroupList(limit: Int, offset: Int): UserGroupListResult!
+}
+
+input CreateUserInput {
+  name: String!
+  deactivated: Boolean!
+  clientMutationId: String
+}
+
+type CreateUserPayload {
+  user: User!
+  clientMutationId: String
+}
+
+union CreateUserPayloadResult =
+    CreateUserPayload
+  | PermissionDeniedError
+  | UnknownRuntimeError
+
+input UpdateUserInput {
+  id: ID!
+  name: String!
+  deactivated: Boolean!
+  clientMutationId: String
+}
+
+type UpdateUserPayload {
+  user: User!
+  clientMutationId: String
+}
+
+union UpdateUserPayloadResult =
+    UpdateUserPayload
+  | PermissionDeniedError
+  | UnknownRuntimeError
+  | NotFoundError
+
+input DeleteUserInput {
+  id: ID!
+  clientMutationId: String
+}
+
+type DeleteUserPayload {
+  id: ID!
+  clientMutationId: String
+}
+
+union DeleteUserPayloadResult =
+    DeleteUserPayload
+  | UnknownRuntimeError
+  | PermissionDeniedError
+  | NotFoundError
+
+input CreateGroupInput {
+  name: String!
+  clientMutationId: String
+}
+
+type CreateGroupPayload {
+  group: Group!
+  clientMutationId: String
+}
+
+union CreateGroupPayloadResult =
+    CreateGroupPayload
+  | PermissionDeniedError
+  | UnknownRuntimeError
+
+input UpdateGroupInput {
+  id: ID!
+  name: String!
+  clientMutationId: String
+}
+
+type UpdateGroupPayload {
+  group: Group!
+  clientMutationId: String
+}
+
+union UpdateGroupPayloadResult =
+    UpdateGroupPayload
+  | PermissionDeniedError
+  | UnknownRuntimeError
+  | NotFoundError
+
+input DeleteGroupInput {
+  id: ID!
+  clientMutationId: String
+}
+
+type DeleteGroupPayload {
+  id: ID!
+  clientMutationId: String
+}
+
+union DeleteGroupPayloadResult =
+    DeleteGroupPayload
+  | UnknownRuntimeError
+  | PermissionDeniedError
+  | NotFoundError
+
+input CreateUserGroupInput {
+  userId: String!
+  groupId: String!
+  clientMutationId: String
+}
+
+type CreateUserGroupPayload {
+  userGroup: UserGroup!
+  clientMutationId: String
+}
+
+union CreateUserGroupPayloadResult =
+    CreateUserGroupPayload
+  | PermissionDeniedError
+  | UnknownRuntimeError
+  | NotFoundError
+
+input UpdateUserGroupInput {
+  id: ID!
+  userId: String!
+  groupId: String!
+  clientMutationId: String
+}
+
+type UpdateUserGroupPayload {
+  userGroup: UserGroup!
+  clientMutationId: String
+}
+
+union UpdateUserGroupPayloadResult =
+    UpdateUserGroupPayload
+  | PermissionDeniedError
+  | UnknownRuntimeError
+  | NotFoundError
+
+input DeleteUserGroupInput {
+  id: ID!
+  clientMutationId: String
+}
+
+type DeleteUserGroupPayload {
+  id: ID!
+  clientMutationId: String
+}
+
+union DeleteUserGroupPayloadResult =
+    DeleteUserGroupPayload
+  | UnknownRuntimeError
+  | PermissionDeniedError
+  | NotFoundError
+
+type Mutation {
+  createUser(input: CreateUserInput!): CreateUserPayloadResult!
+  updateUser(input: UpdateUserInput!): UpdateUserPayloadResult!
+  deleteUser(input: DeleteUserInput!): DeleteUserPayloadResult!
+  createGroup(input: CreateGroupInput!): CreateGroupPayloadResult!
+  updateGroup(input: UpdateGroupInput!): UpdateGroupPayloadResult!
+  deleteGroup(input: DeleteGroupInput!): DeleteGroupPayloadResult!
+  createUserGroup(input: CreateUserGroupInput!): CreateUserGroupPayloadResult!
+  updateUserGroup(input: UpdateUserGroupInput!): UpdateUserGroupPayloadResult!
+  deleteUserGroup(input: DeleteUserGroupInput!): DeleteUserGroupPayloadResult!
+}
+`;
+      const response = await useCase.run(
+        domainEntitiesDirectoryPath,
+        outputGraphqlSchemaPath,
+      );
+
+      expect(response).toEqual(expectedSchema);
+      expect(entityDefinitionRepository.find).toHaveBeenCalledWith(
+        domainEntitiesDirectoryPath,
+      );
+      expect(fileRepository.save).toHaveBeenCalledWith(
+        outputGraphqlSchemaPath,
+        expectedSchema,
+      );
+    });
+  });
+  describe('generateMutation', () => {
+    it('should generate type definitions for all entity definitions', () => {
+      const { useCase } = createUseCaseAndMockRepositories();
+
+      const expectedMutation = `
+input CreateUserInput {
+  name: String!
+  deactivated: Boolean!
+  clientMutationId: String
+}
+
+type CreateUserPayload {
+  user: User!
+  clientMutationId: String
+}
+
+union CreateUserPayloadResult =
+    CreateUserPayload
+  | PermissionDeniedError
+  | UnknownRuntimeError
+
+input UpdateUserInput {
+  id: ID!
+  name: String!
+  deactivated: Boolean!
+  clientMutationId: String
+}
+
+type UpdateUserPayload {
+  user: User!
+  clientMutationId: String
+}
+
+union UpdateUserPayloadResult =
+    UpdateUserPayload
+  | PermissionDeniedError
+  | UnknownRuntimeError
+  | NotFoundError
+
+input DeleteUserInput {
+  id: ID!
+  clientMutationId: String
+}
+
+type DeleteUserPayload {
+  id: ID!
+  clientMutationId: String
+}
+
+union DeleteUserPayloadResult =
+    DeleteUserPayload
+  | UnknownRuntimeError
+  | PermissionDeniedError
+  | NotFoundError
+
+input CreateGroupInput {
+  name: String!
+  clientMutationId: String
+}
+
+type CreateGroupPayload {
+  group: Group!
+  clientMutationId: String
+}
+
+union CreateGroupPayloadResult =
+    CreateGroupPayload
+  | PermissionDeniedError
+  | UnknownRuntimeError
+
+input UpdateGroupInput {
+  id: ID!
+  name: String!
+  clientMutationId: String
+}
+
+type UpdateGroupPayload {
+  group: Group!
+  clientMutationId: String
+}
+
+union UpdateGroupPayloadResult =
+    UpdateGroupPayload
+  | PermissionDeniedError
+  | UnknownRuntimeError
+  | NotFoundError
+
+input DeleteGroupInput {
+  id: ID!
+  clientMutationId: String
+}
+
+type DeleteGroupPayload {
+  id: ID!
+  clientMutationId: String
+}
+
+union DeleteGroupPayloadResult =
+    DeleteGroupPayload
+  | UnknownRuntimeError
+  | PermissionDeniedError
+  | NotFoundError
+
+input CreateUserGroupInput {
+  userId: String!
+  groupId: String!
+  clientMutationId: String
+}
+
+type CreateUserGroupPayload {
+  userGroup: UserGroup!
+  clientMutationId: String
+}
+
+union CreateUserGroupPayloadResult =
+    CreateUserGroupPayload
+  | PermissionDeniedError
+  | UnknownRuntimeError
+  | NotFoundError
+
+input UpdateUserGroupInput {
+  id: ID!
+  userId: String!
+  groupId: String!
+  clientMutationId: String
+}
+
+type UpdateUserGroupPayload {
+  userGroup: UserGroup!
+  clientMutationId: String
+}
+
+union UpdateUserGroupPayloadResult =
+    UpdateUserGroupPayload
+  | PermissionDeniedError
+  | UnknownRuntimeError
+  | NotFoundError
+
+input DeleteUserGroupInput {
+  id: ID!
+  clientMutationId: String
+}
+
+type DeleteUserGroupPayload {
+  id: ID!
+  clientMutationId: String
+}
+
+union DeleteUserGroupPayloadResult =
+    DeleteUserGroupPayload
+  | UnknownRuntimeError
+  | PermissionDeniedError
+  | NotFoundError
+
+type Mutation {
+  createUser(input: CreateUserInput!): CreateUserPayloadResult!
+  updateUser(input: UpdateUserInput!): UpdateUserPayloadResult!
+  deleteUser(input: DeleteUserInput!): DeleteUserPayloadResult!
+  createGroup(input: CreateGroupInput!): CreateGroupPayloadResult!
+  updateGroup(input: UpdateGroupInput!): UpdateGroupPayloadResult!
+  deleteGroup(input: DeleteGroupInput!): DeleteGroupPayloadResult!
+  createUserGroup(input: CreateUserGroupInput!): CreateUserGroupPayloadResult!
+  updateUserGroup(input: UpdateUserGroupInput!): UpdateUserGroupPayloadResult!
+  deleteUserGroup(input: DeleteUserGroupInput!): DeleteUserGroupPayloadResult!
+}
+`;
+
+      const mutation = useCase.generateMutation(entityDefinitions);
+
+      expect(mutation.trim()).toEqual(expectedMutation.trim());
+    });
+  });
+  describe('generateTypes', () => {
+    it('should generate type definitions for all entity definitions', () => {
+      const { useCase } = createUseCaseAndMockRepositories();
+
+      const expectedTypeDefs = `
+type User {
+  id: String!
+  name: String!
+  deactivated: Boolean!
+  userGroupList: [UserGroupListResult!]!
+}
+
+type Group {
+  id: String!
+  name: String!
+  userGroupList: [UserGroupListResult!]!
+}
+
+type UserGroup {
+  id: String!
+  userId: String!
+  user: UserResult!
+  groupId: String!
+  group: GroupResult!
+}
+
+`;
+
+      const typeDefs = useCase.generateTypes(entityDefinitions);
+
+      expect(typeDefs.trim()).toEqual(expectedTypeDefs.trim());
+    });
+  });
+  describe('generateErrorTypes', () => {
+    it('should generate error type definitions', () => {
+      const { useCase } = createUseCaseAndMockRepositories();
+
+      const expectedTypeDefs = `
+enum ErrorCode {
+  UNKNOWN_RUNTIME
+  PERMISSION_DENIED
+  NOT_FOUND
+}
+
+type UnknownRuntimeError {
+  errorCode: ErrorCode!
+  message: String
+  stack: String
+}
+
+type PermissionDeniedError {
+  errorCode: ErrorCode!
+  message: String
+  stack: String
+}
+
+type NotFoundError {
+  errorCode: ErrorCode!
+  message: String
+  stack: String
+}
+
+`;
+
+      const typeDefs = useCase.generateErrorTypes();
+
+      expect(typeDefs.trim()).toEqual(expectedTypeDefs.trim());
+    });
+  });
+  describe('generateQuery', () => {
+    it('should generate query definitions for all entity definitions', () => {
+      const { useCase } = createUseCaseAndMockRepositories();
+
+      const expectedQueryDefs = `
+union UserResult =
+    User
+  | NotFoundError
+  | PermissionDeniedError
+  | UnknownRuntimeError
+
+type UserList {
+  items: [User!]!
+  total: Int!
+}
+
+union UserListResult =
+    UserList
+  | NotFoundError
+  | PermissionDeniedError
+  | UnknownRuntimeError
+
+union GroupResult =
+    Group
+  | NotFoundError
+  | PermissionDeniedError
+  | UnknownRuntimeError
+
+type GroupList {
+  items: [Group!]!
+  total: Int!
+}
+
+union GroupListResult =
+    GroupList
+  | NotFoundError
+  | PermissionDeniedError
+  | UnknownRuntimeError
+
+union UserGroupResult =
+    UserGroup
+  | NotFoundError
+  | PermissionDeniedError
+  | UnknownRuntimeError
+
+type UserGroupList {
+  items: [UserGroup!]!
+  total: Int!
+}
+
+union UserGroupListResult =
+    UserGroupList
+  | NotFoundError
+  | PermissionDeniedError
+  | UnknownRuntimeError
+
+type Query {
+  user(id: ID!): UserResult!
+  userList(limit: Int, offset: Int): UserListResult!
+  group(id: ID!): GroupResult!
+  groupList(limit: Int, offset: Int): GroupListResult!
+  userGroup(id: ID!): UserGroupResult!
+  userGroupList(limit: Int, offset: Int): UserGroupListResult!
+} 
+`;
+
+      const queryDefs = useCase.generateQuery(entityDefinitions);
+
+      expect(queryDefs.trim()).toEqual(expectedQueryDefs.trim());
+    });
+  });
+
+  describe('mapToGraphQLType', () => {
+    it('should map "boolean" to "Boolean"', () => {
+      const { useCase } = createUseCaseAndMockRepositories();
+      expect(useCase.mapToGraphQLType('boolean')).toEqual('Boolean');
+    });
+
+    it('should map "number" to "Int"', () => {
+      const { useCase } = createUseCaseAndMockRepositories();
+      expect(useCase.mapToGraphQLType('number')).toEqual('Int');
+    });
+
+    it('should map "string" to "String"', () => {
+      const { useCase } = createUseCaseAndMockRepositories();
+      expect(useCase.mapToGraphQLType('string')).toEqual('String');
+    });
+
+    it('should map "Date" to "Date"', () => {
+      const { useCase } = createUseCaseAndMockRepositories();
+      expect(useCase.mapToGraphQLType('Date')).toEqual('Date');
+    });
+  });
+  describe('capitalize', () => {
+    it('should capitalize the first letter of a non-empty string', () => {
+      const { useCase } = createUseCaseAndMockRepositories();
+      expect(useCase.capitalize('hello')).toBe('Hello');
+      expect(useCase.capitalize('world')).toBe('World');
+      expect(useCase.capitalize('JavaScript')).toBe('JavaScript');
+    });
+
+    it('should return an empty string for an empty string input', () => {
+      const { useCase } = createUseCaseAndMockRepositories();
+      expect(useCase.capitalize('')).toBe('');
+    });
+  });
+
+  describe('uncapitalize', () => {
+    it('should return an empty string when given an empty string', () => {
+      const { useCase } = createUseCaseAndMockRepositories();
+      const input = '';
+      const result = useCase.uncapitalize(input);
+      expect(result).toBe('');
+    });
+
+    it('should convert the first character to lowercase', () => {
+      const { useCase } = createUseCaseAndMockRepositories();
+      const input = 'Hello World!';
+
+      const result = useCase.uncapitalize(input);
+      expect(result).toBe('hello World!');
+    });
+
+    it('should not modify the string if the first character is already lowercase', () => {
+      const { useCase } = createUseCaseAndMockRepositories();
+      const input = 'hello World!';
+      const result = useCase.uncapitalize(input);
+      expect(result).toBe('hello World!');
+    });
+  });
+  describe('snakeCaseToPascalCase', () => {
+    it('converts a snake_case string to PascalCase', () => {
+      const { useCase } = createUseCaseAndMockRepositories();
+      expect(useCase.snakeCaseToPascalCase('PERMISSION_DENIED')).toBe(
+        'PermissionDenied',
+      );
+      expect(useCase.snakeCaseToPascalCase('SOME_OTHER_VALUE')).toBe(
+        'SomeOtherValue',
+      );
+      expect(useCase.snakeCaseToPascalCase('YET_ANOTHER_NAME')).toBe(
+        'YetAnotherName',
+      );
+      expect(useCase.snakeCaseToPascalCase('a_b_c')).toBe('ABC');
+      expect(useCase.snakeCaseToPascalCase('permission_denied')).toBe(
+        'PermissionDenied',
+      );
+    });
+  });
+
+  const createUseCaseAndMockRepositories = () => {
+    const fileRepository = createMockFileRepository();
+    const entityDefinitionRepository =
+      createMockEntityRelationDefinitionRepository();
+    const useCase = new GenerateGraphqlSchemaUseCase(
+      fileRepository,
+      entityDefinitionRepository,
+    );
+    return {
+      fileRepository,
+      entityDefinitionRepository,
+      useCase,
+    };
+  };
+
+  const createMockFileRepository = () => {
+    const repository: FileRepository = {
+      save: jest.fn(),
+    };
+    return {
+      save: jest.fn((path: string, content: string) =>
+        repository.save(path, content),
+      ),
+    };
+  };
+
+  const createMockEntityRelationDefinitionRepository = () => {
+    const repository: EntityDefinitionRepository = {
+      find: async (_path: string): Promise<EntityDefinition[]> => {
+        return [];
+      },
+    };
+    return {
+      find: jest.fn((path: string) => repository.find(path)),
+    };
+  };
+});
---
+++ src/domain/usecases/GenerateGraphqlSchemaUseCase.ts
@@ -0,0 +1,279 @@
+import { FileRepository } from './adapter-interfaces/FileRepository';
+import { EntityDefinitionRepository } from './adapter-interfaces/EntityRelationDefinitionRepository';
+import { EntityDefinition } from '../entities/EntityDefinition';
+import { shouldBeNever } from '../../_shared/utility';
+import {
+  EntityPropertyDefinitionPrimitive,
+  EntityPropertyDefinitionReferencedObject,
+} from '../entities/EntityPropertyDefinition';
+const ERROR_TYPES = ['UNKNOWN_RUNTIME', 'PERMISSION_DENIED', 'NOT_FOUND'];
+
+export class GenerateGraphqlSchemaUseCase {
+  constructor(
+    readonly fileRepository: FileRepository,
+    readonly entityDefinitionRepository: EntityDefinitionRepository,
+  ) {}
+
+  run = async (
+    domainEntitiesDirectoryPath: string,
+    outputGraphqlSchemaPath: string | null,
+  ): Promise<string> => {
+    const entityDefinitions: EntityDefinition[] =
+      await this.entityDefinitionRepository.find(domainEntitiesDirectoryPath);
+
+    const schema = `${this.generateErrorTypes()}
+${this.generateTypes(entityDefinitions)}
+${this.generateQuery(entityDefinitions)}
+${this.generateMutation(entityDefinitions)}
+`;
+    if (outputGraphqlSchemaPath) {
+      await this.fileRepository.save(outputGraphqlSchemaPath, schema);
+    }
+    return schema;
+  };
+  generateTypes = (entityDefinitions: EntityDefinition[]): string => {
+    const typeDefs: string[] = [];
+
+    for (const entity of entityDefinitions) {
+      const properties: string[] = [];
+
+      for (const property of entity.properties) {
+        const name = property.name;
+
+        const type = property.isReference
+          ? 'String!'
+          : this.mapToGraphQLType(property.propertyType) + '!';
+        properties.push(`  ${name}: ${type}`);
+        if (property.isReference) {
+          properties.push(
+            `  ${this.uncapitalize(property.propertyType)}: ${
+              property.propertyType
+            }Result!`,
+          );
+        }
+      }
+
+      const relatedEntities = entityDefinitions.flatMap((relatedEntity) =>
+        relatedEntity.properties
+          .filter(
+            (
+              relatedProperty,
+            ): relatedProperty is EntityPropertyDefinitionReferencedObject =>
+              relatedProperty.isReference &&
+              relatedProperty.propertyType === entity.typeName,
+          )
+          .map((relatedProperty) =>
+            relatedProperty.isUnique
+              ? `  ${this.uncapitalize(relatedEntity.typeName)}: ${
+                  relatedEntity.typeName
+                }!`
+              : `  ${this.uncapitalize(relatedEntity.typeName)}List: [${
+                  relatedEntity.typeName
+                }ListResult!]!`,
+          ),
+      );
+
+      typeDefs.push(`type ${entity.typeName} {
+${properties.join('\n')}${
+        relatedEntities.length > 0 ? '\n' + relatedEntities.join('\n') : ``
+      }
+}
+
+`);
+    }
+
+    return typeDefs.join('');
+  };
+  capitalize = (str: string): string => {
+    if (str.length === 0) {
+      return str;
+    }
+    return str[0].toUpperCase() + str.slice(1);
+  };
+
+  uncapitalize = (str: string): string => {
+    return str.charAt(0).toLowerCase() + str.slice(1);
+  };
+  generateQuery = (entityDefinitions: EntityDefinition[]): string => {
+    const resultTypes: string[] = [];
+    for (const entity of entityDefinitions) {
+      const resultType = `union ${entity.typeName}Result =
+    ${entity.typeName}
+  | NotFoundError
+  | PermissionDeniedError
+  | UnknownRuntimeError
+
+type ${entity.typeName}List {
+  items: [${entity.typeName}!]!
+  total: Int!
+}
+
+union ${entity.typeName}ListResult =
+    ${entity.typeName}List
+  | NotFoundError
+  | PermissionDeniedError
+  | UnknownRuntimeError
+`;
+      resultTypes.push(resultType);
+    }
+
+    const queryMethods = entityDefinitions.map((entity) => {
+      return `  ${this.uncapitalize(entity.typeName)}(id: ID!): ${
+        entity.typeName
+      }Result!
+  ${this.uncapitalize(entity.typeName)}List(limit: Int, offset: Int): ${
+        entity.typeName
+      }ListResult!`;
+    });
+
+    //   const queryMethods: string[] = [];
+    //   for (const entity of entityDefinitions) {
+    //     const queryMethod = `  ${this.uncapitalize(entity.typeName)}(id: ID!): ${
+    //       entity.typeName
+    //     }Result!
+    // ${this.uncapitalize(entity.typeName)}List(limit: Int, offset: Int): ${
+    //       entity.typeName
+    //     }ListResult!`;
+    //     queryMethods.push(queryMethod);
+    //   }
+    const query = `${resultTypes.join(`\n`)}
+type Query {
+${queryMethods.join('\n')}
+}
+`;
+    return query;
+  };
+  generateErrorTypes = (): string => {
+    const errors = ERROR_TYPES;
+    const errorTypes: string[] = [];
+    for (const error of ERROR_TYPES) {
+      errorTypes.push(`type ${this.snakeCaseToPascalCase(error)}Error {
+  errorCode: ErrorCode!
+  message: String
+  stack: String
+}
+`);
+    }
+    return `
+enum ErrorCode {
+${errors.map((error) => `  ${error}`).join(`\n`)}
+}
+
+${errorTypes.join('\n')}`;
+  };
+  generateMutation = (entityDefinitions: EntityDefinition[]): string => {
+    const mutationTypes: string[] = [];
+    for (const entity of entityDefinitions) {
+      const uncapitalizedEntityName = this.uncapitalize(entity.typeName);
+      const properties = entity.properties
+        .filter((property) => property.name != 'id')
+        .map((property) =>
+          property.isReference
+            ? `  ${this.uncapitalize(property.propertyType)}Id: String!`
+            : `  ${property.name}: ${this.mapToGraphQLType(
+                property.propertyType,
+              )}!`,
+        )
+        .join('\n');
+      const notFoundError = entity.properties.some(
+        (property) => property.isReference,
+      )
+        ? '\n  | NotFoundError'
+        : '';
+
+      const mutationType = `input Create${entity.typeName}Input {
+${properties}
+  clientMutationId: String
+}
+
+type Create${entity.typeName}Payload {
+  ${uncapitalizedEntityName}: ${entity.typeName}!
+  clientMutationId: String
+}
+
+union Create${entity.typeName}PayloadResult =
+    Create${entity.typeName}Payload
+  | PermissionDeniedError
+  | UnknownRuntimeError${notFoundError}
+
+input Update${entity.typeName}Input {
+  id: ID!
+${properties}
+  clientMutationId: String
+}
+
+type Update${entity.typeName}Payload {
+  ${uncapitalizedEntityName}: ${entity.typeName}!
+  clientMutationId: String
+}
+
+union Update${entity.typeName}PayloadResult =
+    Update${entity.typeName}Payload
+  | PermissionDeniedError
+  | UnknownRuntimeError
+  | NotFoundError
+
+input Delete${entity.typeName}Input {
+  id: ID!
+  clientMutationId: String
+}
+
+type Delete${entity.typeName}Payload {
+  id: ID!
+  clientMutationId: String
+}
+
+union Delete${entity.typeName}PayloadResult =
+    Delete${entity.typeName}Payload
+  | UnknownRuntimeError
+  | PermissionDeniedError
+  | NotFoundError
+`;
+      mutationTypes.push(mutationType);
+    }
+
+    const mutations = entityDefinitions.map((entity) => {
+      return `  create${entity.typeName}(input: Create${entity.typeName}Input!): Create${entity.typeName}PayloadResult!
+  update${entity.typeName}(input: Update${entity.typeName}Input!): Update${entity.typeName}PayloadResult!
+  delete${entity.typeName}(input: Delete${entity.typeName}Input!): Delete${entity.typeName}PayloadResult!`;
+    });
+
+    //   const mutations: string[] = [];
+    //   for (const entity of entityDefinitions) {
+    //     const mutation = `  create${entity.typeName}(input: Create${entity.typeName}Input!): Create${entity.typeName}PayloadResult!
+    // update${entity.typeName}(input: Update${entity.typeName}Input!): Update${entity.typeName}PayloadResult!
+    // delete${entity.typeName}(input: Delete${entity.typeName}Input!): Delete${entity.typeName}PayloadResult!`;
+    //     mutations.push(mutation);
+    //   }
+    const mutation = `${mutationTypes.join('\n')}
+type Mutation {
+${mutations.join('\n')}
+}`;
+    return mutation;
+  };
+  mapToGraphQLType = (
+    propertyType: EntityPropertyDefinitionPrimitive['propertyType'],
+  ): string => {
+    switch (propertyType) {
+      case 'boolean':
+        return 'Boolean';
+      case 'number':
+        return 'Int';
+      case 'string':
+        return 'String';
+      case 'Date':
+        return 'Date';
+      default:
+        shouldBeNever(propertyType);
+        throw new Error(
+          `Invalid property type: ${JSON.stringify(propertyType)}`,
+        );
+    }
+  };
+  snakeCaseToPascalCase = (str: string): string => {
+    return str
+      .toLowerCase()
+      .replace(/_(\w)/g, (_, letter: string) => letter.toUpperCase())
+      .replace(/^\w/, (letter) => letter.toUpperCase());
+  };
+}
