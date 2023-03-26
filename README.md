8ce8fee5e39b8a3c7baf1100fa64bedf874b252a
HiromiShikata <shikata.hiromi@gmail.com>
2023-03-26 20:34:30 +0000

    feat: make cli entry point

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 [![CircleCI](https://dl.circleci.com/status-badge/img/gh/HiromiShikata/graphql-generator-from-entity-definitions/tree/main.svg?style=svg)](https://dl.circleci.com/status-badge/redirect/gh/HiromiShikata/graphql-generator-from-entity-definitions/tree/main)
 [![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
 [![semantic-release: angular](https://img.shields.io/badge/semantic--release-angular-e10079?logo=semantic-release)](https://github.com/semantic-release/semantic-release)
+
+## Usage
+
+```
+npx graphql-generator-from-entity-definitions /path/to/domain/entity/dir -o /path/to/schema.graphql
+```
+
+## Limitation
+
+- Domain entity should be defined using `type`
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-export const hello = (name: string) => `hello ${name}`;
+//./src/index.ts
+import { Command } from 'commander';
+import { GenerateGraphqlSchemaUseCase } from './domain/usecases/GenerateGraphqlSchemaUseCase';
+import { FsFileRepository } from './adapter/repositories/FsFileRepository';
+import { AstEntityDefinitionRepository } from './adapter/repositories/AstEntityDefinitionRepository';
+
+const program = new Command();
+program
+  .name('GraphQL Generator')
+  .argument('<inputPath>', 'Path to domain entities dir.')
+  .option(`-o, --output <outputPath>`, 'Path to schema.graphql.')
+  .description('Generate GraphQL schema from entity definitions')
+  .action(async (inputPath: string, options: { outputPath: string }) => {
+    const useCase = new GenerateGraphqlSchemaUseCase(
+      new FsFileRepository(),
+      new AstEntityDefinitionRepository(),
+    );
+    const res = await useCase.run(inputPath, options.outputPath);
+    if (!options.outputPath) {
+      console.log(res);
+    }
+  });
+program.parse(process.argv);
