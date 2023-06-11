75e4b5d395c521285d28e153247d80fd0f3c576f
HiromiShikata <shikata.hiromi@gmail.com>
2023-06-11 20:42:30 +0000

    fix: ENOENT: no such file or directory, about bin/index.js

---
+++ bin/_shared/utility.js
@@ -0,0 +1,6 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+exports.shouldBeNever = void 0;
+const shouldBeNever = (value) => console.log(value);
+exports.shouldBeNever = shouldBeNever;
+//# sourceMappingURL=utility.js.map
