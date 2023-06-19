a97791be157598fe5e685befa07709046638881b
HiromiShikata <shikata.hiromi@gmail.com>
2023-06-19 00:18:48 +0000

    autogen: build
    npm run build

--- bin/adapter/repositories/AstEntityDefinitionRepository.js
+++ bin/adapter/repositories/AstEntityDefinitionRepository.js
@@ -1,13 +1,13 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.AstEntityDefinitionRepository = void 0;
-const index_1 = require('ast-to-entity-definitions/bin/adapter/entry-points/function/index');
+const index_1 = require("ast-to-entity-definitions/bin/adapter/entry-points/function/index");
 class AstEntityDefinitionRepository {
-  constructor() {
-    this.find = async (path) => {
-      return await (0, index_1.getEntityDefinitions)(path);
-    };
-  }
+    constructor() {
+        this.find = async (path) => {
+            return await (0, index_1.getEntityDefinitions)(path);
+        };
+    }
 }
 exports.AstEntityDefinitionRepository = AstEntityDefinitionRepository;
 //# sourceMappingURL=AstEntityDefinitionRepository.js.map
