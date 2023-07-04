9021b7e4f966b3c60a715147111d0850e59ae468
HiromiShikata <HiromiShikata@users.noreply.github.com>
2023-07-04 20:34:41 +0000

    chore: re-build / npm run build

--- bin/adapter/repositories/ChangeCaseStringConvertor.js
+++ bin/adapter/repositories/ChangeCaseStringConvertor.js
@@ -1,28 +1,28 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.ChangeCaseStringConvertor = void 0;
-const change_case_all_1 = require('change-case-all');
+const change_case_all_1 = require("change-case-all");
 class ChangeCaseStringConvertor {
-  constructor() {
-    this.camelCase = (str) => {
-      return (0, change_case_all_1.camelCase)(str);
-    };
-    this.snakeCase = (str) => {
-      return (0, change_case_all_1.snakeCase)(str);
-    };
-    this.pascalCase = (str) => {
-      return (0, change_case_all_1.pascalCase)(str);
-    };
-    this.kebabCase = (str) => {
-      return (0, change_case_all_1.paramCase)(str);
-    };
-    this.screamSnakeCase = (str) => {
-      return (0, change_case_all_1.snakeCase)(str).toUpperCase();
-    };
-    this.paramCase = (str) => {
-      return (0, change_case_all_1.paramCase)(str);
-    };
-  }
+    constructor() {
+        this.camelCase = (str) => {
+            return (0, change_case_all_1.camelCase)(str);
+        };
+        this.snakeCase = (str) => {
+            return (0, change_case_all_1.snakeCase)(str);
+        };
+        this.pascalCase = (str) => {
+            return (0, change_case_all_1.pascalCase)(str);
+        };
+        this.kebabCase = (str) => {
+            return (0, change_case_all_1.paramCase)(str);
+        };
+        this.screamSnakeCase = (str) => {
+            return (0, change_case_all_1.snakeCase)(str).toUpperCase();
+        };
+        this.paramCase = (str) => {
+            return (0, change_case_all_1.paramCase)(str);
+        };
+    }
 }
 exports.ChangeCaseStringConvertor = ChangeCaseStringConvertor;
 //# sourceMappingURL=ChangeCaseStringConvertor.js.map
--- bin/index.js
+++ bin/index.js
@@ -1,45 +1,27 @@
 #!/usr/bin/env node
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 //./src/index.ts
-const commander_1 = require('commander');
-const GenerateGraphqlSchemaUseCase_1 = require('./domain/usecases/GenerateGraphqlSchemaUseCase');
-const FsFileRepository_1 = require('./adapter/repositories/FsFileRepository');
-const AstEntityDefinitionRepository_1 = require('./adapter/repositories/AstEntityDefinitionRepository');
-const ChangeCaseStringConvertor_1 = require('./adapter/repositories/ChangeCaseStringConvertor');
+const commander_1 = require("commander");
+const GenerateGraphqlSchemaUseCase_1 = require("./domain/usecases/GenerateGraphqlSchemaUseCase");
+const FsFileRepository_1 = require("./adapter/repositories/FsFileRepository");
+const AstEntityDefinitionRepository_1 = require("./adapter/repositories/AstEntityDefinitionRepository");
+const ChangeCaseStringConvertor_1 = require("./adapter/repositories/ChangeCaseStringConvertor");
 const commaSeparatedList = (value, _previous) => {
-  return value.split(',');
+    return value.split(',');
 };
 const program = new commander_1.Command();
 program
-  .name('GraphQL Generator')
-  .argument('<inputPath>', 'Path to domain entities dir.')
-  .option(`-o, --outputPath <outputPath>`, 'Path to schema.graphql.')
-  .option(
-    `--ignorePropertyNamesForCreation <ignorePropertyNamesForCreation>`,
-    'Option to specify property names to be ignored when creating an object from the command line.',
-    commaSeparatedList,
-  )
-  .option(
-    `--ignorePropertyNamesForUpdate <ignorePropertyNamesForUpdate>`,
-    'Option to specify property names to be ignored when updating an object from the command line.',
-    commaSeparatedList,
-  )
-  .description('Generate GraphQL schema from entity definitions')
-  .action(async (inputPath, options) => {
-    const useCase =
-      new GenerateGraphqlSchemaUseCase_1.GenerateGraphqlSchemaUseCase(
-        new FsFileRepository_1.FsFileRepository(),
-        new AstEntityDefinitionRepository_1.AstEntityDefinitionRepository(),
-        new ChangeCaseStringConvertor_1.ChangeCaseStringConvertor(),
-      );
-    const res = await useCase.run(
-      inputPath,
-      options.ignorePropertyNamesForCreation ?? [],
-      options.ignorePropertyNamesForUpdate ?? [],
-      options.outputPath ?? null,
-    );
+    .name('GraphQL Generator')
+    .argument('<inputPath>', 'Path to domain entities dir.')
+    .option(`-o, --outputPath <outputPath>`, 'Path to schema.graphql.')
+    .option(`--ignorePropertyNamesForCreation <ignorePropertyNamesForCreation>`, 'Option to specify property names to be ignored when creating an object from the command line.', commaSeparatedList)
+    .option(`--ignorePropertyNamesForUpdate <ignorePropertyNamesForUpdate>`, 'Option to specify property names to be ignored when updating an object from the command line.', commaSeparatedList)
+    .description('Generate GraphQL schema from entity definitions')
+    .action(async (inputPath, options) => {
+    const useCase = new GenerateGraphqlSchemaUseCase_1.GenerateGraphqlSchemaUseCase(new FsFileRepository_1.FsFileRepository(), new AstEntityDefinitionRepository_1.AstEntityDefinitionRepository(), new ChangeCaseStringConvertor_1.ChangeCaseStringConvertor());
+    const res = await useCase.run(inputPath, options.ignorePropertyNamesForCreation ?? [], options.ignorePropertyNamesForUpdate ?? [], options.outputPath ?? null);
     console.log(res);
-  });
+});
 program.parse(process.argv);
 //# sourceMappingURL=index.js.map
